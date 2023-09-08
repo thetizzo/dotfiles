0e1c551eb7ef78d6d0560c224caaf60688420b1a
thetizzo <j.m.taylor1@gmail.com>
2023-09-08 13:44:42 -0600

    Replace nvm with n

--- node/install.sh
+++ node/install.sh
@@ -5,12 +5,7 @@
 if test ! $(which n)
 then
   echo "  Installing n node version manager"
-  if test $(which brew)
-  then
-    brew install n
-  else
-    curl -L https://bit.ly/n-install | bash
-  fi
+  curl -L https://bit.ly/n-install | bash -s -- -y
 fi
 
 exit 0
