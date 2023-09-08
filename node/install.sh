bfe89d14427359252370d2842c77884af000f112
thetizzo <j.m.taylor1@gmail.com>
2023-09-08 13:44:42 -0600

    Revert "Replace nvm with n"
    This reverts commit 0e1c551eb7ef78d6d0560c224caaf60688420b1a.
    
    Turns out n kind of sucks

--- node/install.sh
+++ node/install.sh
@@ -5,7 +5,12 @@
 if test ! $(which n)
 then
   echo "  Installing n node version manager"
-  curl -L https://bit.ly/n-install | bash -s -- -y
+  if test $(which brew)
+  then
+    brew install n
+  else
+    curl -L https://bit.ly/n-install | bash
+  fi
 fi
 
 exit 0
