1d8f398a7b057bac7d31d4d52a31b515a9d2e6cc
thetizzo <j.m.taylor1@gmail.com>
2026-04-13 23:54:12 -0600

    Fix stow install if no conflicts are found

--- utils/stow_conflict_free_install.sh
+++ utils/stow_conflict_free_install.sh
@@ -5,7 +5,8 @@ stow_install() {
   STOW_DIR=$TIZZO_PATH/dotfiles
   PACKAGES=($(ls -d $STOW_DIR/*/  | xargs -I{} basename {}))
 
-  conflicts=$(stow -n --dir=$STOW_DIR --target=$STOW_TARGET "${PACKAGES[@]}" 2>&1 | grep -oP 'existing target \K\S+')
+  # || true keeps the script from exiting early if no conflicts are found
+  conflicts=$(stow -n --dir=$STOW_DIR --target=$STOW_TARGET "${PACKAGES[@]}" 2>&1 | grep -oP 'existing target \K\S+' || true)
 
   # Resolve conflicts
   for f in $conflicts; do
@@ -14,5 +15,5 @@ stow_install() {
   done
 
   # Install dotfiles with stow
-  stow --dir="$STOW_DIR" --target="$STOW_TARGET" "${PACKAGES[@]}" 2>&1
+  stow -v --dir="$STOW_DIR" --target="$STOW_TARGET" "${PACKAGES[@]}" 2>&1
 }
